d2d745c19a6b5bc70d5b1d32b120fff478b7125b
Lucas Leandro <lucasleandro1204@gmail.com>
2017-10-20 02:26:07 -0200

    working? im not sure

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <article class="markdown-body">
-    <h1>kkkkkkkkk</h1>
+    <h1>Vue Content Loading</h1>
+
+    <h3>Vue component to easily build (or use presets) SVG loading cards Facebook like.</h3>
+
+    <blockquote>
+      Inspired in <a href="https://github.com/danilowoz/react-content-loader" target="_blank">React Content Loader</a>
+    </blockquote>
+
+    <vue-content-loading></vue-content-loading>
   </article>
 </template>
 
 <script>
-  export default {
+  import VueContentLoading from './components/VueContentLoading.js';
 
+  export default {
+    components: {
+      VueContentLoading,
+    },
   };
 </script>
 
--- src/components/VueContentLoading.js
+++ src/components/VueContentLoading.js
@@ -1,8 +1,76 @@
-import Rect from './custom/Rect.js';
-import Circle from './custom/Circle.js';
+import VclRect from './custom/Rect.js';
+import VclCircle from './custom/Circle.js';
 
 export default {
-  
+  components: {
+    VclRect, VclCircle,
+  },
+
+  props: {
+    type: {
+      default: 'facebook',
+      type: String,
+    },
+    speed: {
+      default: 2,
+      type: Number,
+    },
+    width: {
+      default: 150,
+      type: Number,
+    },
+    height: {
+      default: 150,
+      type: Number,
+    },
+    custom: Object,
+    primary: {
+      type: String,
+      default: '#f00',
+    },
+    secondary: {
+      type: String,
+      default: '#0f0',
+    },
+  },
+
+  computed: {
+    viewbox () {
+      return `0 0 ${this.width} ${this.height}`;
+    },
+
+    formatedSpeed () {
+      return this.speed + 's';
+    },
+  },
+
+  template: `
+    <svg :viewBox="viewbox" :style="custom" preserveAspectRatio="xMidYMid meet">
+      <rect style="fill: url(#gradient)" clip-path="url(#path)" x="0" y="0" :width="width" :height="height" />
+
+      <defs>
+        <clipPath id="path">
+          <slot>
+            <circle cx="30" cy="30" r="20" />
+          </slot>
+        </clipPath>
+
+        <linearGradient id="gradient">
+          <stop offset="0%" :stop-color="primary">
+            <animate attributeName="offset" values="-2; 1" :dur="formatedSpeed" repeatCount="indefinite" />
+          </stop>
+
+          <stop offset="50%" :stop-color="secondary">
+            <animate attributeName="offset" values="-1.5; 1.5" :dur="formatedSpeed" repeatCount="indefinite" />
+          </stop>
+
+          <stop offset="100%" :stop-color="primary">
+            <animate attributeName="offset" values="-1; 2" :dur="formatedSpeed" repeatCount="indefinite" />
+          </stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  `,
 };
 
 export {
