6f921dd691f36331154eda906433627f1538ab84
Lucas Leandro <lucasleandro1204@gmail.com>
2017-10-24 00:45:17 -0200

    instagram

--- src/core/components.js
+++ src/core/components.js
@@ -1,9 +1,11 @@
 import VueContentLoading from '../components/VueContentLoading.js';
 
 import VclFacebook from '../components/presets/Facebook.js';
+import VclInstagram from '../components/presets/Instagram.js';
 
 export default VueContentLoading;
 
 export {
   VclFacebook,
+  VclInstagram,
 };
