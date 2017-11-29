7a338fdf7f0c893d517db84d69181a1e5baa13dc
Lucas Leandro <lucasleandro1204@gmail.com>
2017-11-29 21:37:11 -0200

    refact

--- src/components/presets/BulletList.js
+++ src/components/presets/BulletList.js
@@ -7,21 +7,27 @@ export default {
 
   data () {
     return {
-      range: [0, 1, 2, 3, 4],
+      rows: 5,
     };
   },
 
   computed: {
     height () {
-      return this.range.length * 21;
+      return this.rows * 21;
+    },
+  },
+
+  methods: {
+    getYPos(row, plus) {
+      return plus + ((row - 1) * 22);
     },
   },
 
   template: `
     <vue-content-loading v-bind="$attrs" :width="230" :height="height">
-      <template v-for="i in range">
-        <circle cx="8" :cy="8 + (i * 22)" r="8" />
-        <rect x="22" :y="3 + (i * 22)" rx="3" ry="3" width="200" height="9" />
+      <template v-for="i in rows">
+        <circle cx="8" :cy="getYPos(i, 8)" r="8" />
+        <rect x="22" :y="getYPos(i, 3)" rx="3" ry="3" width="200" height="9" />
       </template>
     </vue-content-loading>
   `,
