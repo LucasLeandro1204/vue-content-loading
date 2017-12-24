92b5654e17078b25101d0b763b8b38c7826cc2d9
Lucas Leandro <lucasleandro1204@gmail.com>
2017-12-23 22:42:32 -0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,11 @@ var webpack = require('webpack')
 
 module.exports = {
   entry: {
-    app: './src/main.js',
-    vuecontentloading: './src/core/components.js',
+    '../docs/assets/js/app': './src/app.js',
+     vuecontentloading: './src/core/components.js'
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
     filename: '[name].js',
     library: 'VueContentLoading',
     libraryTarget: 'umd'
@@ -20,13 +19,9 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
             'scss': 'vue-style-loader!css-loader!sass-loader',
             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
-          // other vue-loader options go here
         }
       },
       {
@@ -61,7 +56,6 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
