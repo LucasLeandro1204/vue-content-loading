220a3aea79cb1c0973ad3f0a9b5a2024a0eb662f
Marvin <marvin.semmelroth@gmail.com>
2018-08-02 07:18:14 +0200

    fixed formatting

--- src/components/presets/BulletList.vue
+++ src/components/presets/BulletList.vue
@@ -1,37 +1,33 @@
 <template>
-	<vue-content-loading v-bind="$attrs" :width="230" :height="height">
-	  <template v-for="i in rows">
-		<circle cx="8" :cy="getYPos(i, 8)" r="8" :key="i + '_c'" />
-		<rect x="22" :y="getYPos(i, 3)" rx="3" ry="3" width="200" height="9" :key="i + '_r'" />
-	  </template>
-	</vue-content-loading>
+  <vue-content-loading v-bind="$attrs" :width="230" :height="height">
+    <template v-for="i in rows">
+      <circle :key="i + '_c'" cx="8" :cy="getYPos(i, 8)" r="8"  />
+      <rect :key="i + '_r'" x="22" :y="getYPos(i, 3)" rx="3" ry="3" width="200" height="9" />
+    </template>
+  </vue-content-loading>
 </template>
 
 <script>
-import VueContentLoading from 'vue-content-loading';
-
-export default {
-  components: {
-    VueContentLoading,
-  },
-
-  props: {
-    rows: {
-      default: 5,
-      type: Number,
+  import VueContentLoading from '../VueContentLoading.vue';
+  export default {
+    components: {
+      VueContentLoading,
     },
-  },
-
-  computed: {
-    height() {
-      return this.rows * 21;
+    props: {
+      rows: {
+        default: 5,
+        type: Number,
+      },
     },
-  },
-
-  methods: {
-    getYPos(row, plus) {
-      return plus + ((row - 1) * 22);
+    computed: {
+      height () {
+        return this.rows * 21;
+      },
     },
-  },
-};
+    methods: {
+      getYPos (row, plus) {
+        return plus + ((row - 1) * 22);
+      },
+    },
+  };
 </script>
