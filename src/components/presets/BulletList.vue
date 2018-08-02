7a12a9a6f2e0e7552c820bd367cc77b5b91a1f5d
Marvin <marvin.semmelroth@gmail.com>
2018-08-02 07:12:40 +0200

    Fix: Iteration key now for inner components

--- src/components/presets/BulletList.vue
+++ src/components/presets/BulletList.vue
@@ -1,37 +1,37 @@
 <template>
-  <vue-content-loading v-bind="$attrs" :width="230" :height="height">
-    <template v-for="i in rows" :key="i">
-      <circle :key="i" cx="8" :cy="getYPos(i, 8)" r="8" />
-      <rect :key="i" x="22" :y="getYPos(i, 3)" rx="3" ry="3" width="200" height="9" />
-    </template>
-  </vue-content-loading>
+	<vue-content-loading v-bind="$attrs" :width="230" :height="height">
+	  <template v-for="i in rows">
+		<circle cx="8" :cy="getYPos(i, 8)" r="8" :key="i + '_c'" />
+		<rect x="22" :y="getYPos(i, 3)" rx="3" ry="3" width="200" height="9" :key="i + '_r'" />
+	  </template>
+	</vue-content-loading>
 </template>
 
 <script>
-  import VueContentLoading from '../VueContentLoading.vue';
+import VueContentLoading from 'vue-content-loading';
 
-  export default {
-    components: {
-      VueContentLoading,
-    },
+export default {
+  components: {
+    VueContentLoading,
+  },
 
-    props: {
-      rows: {
-        default: 5,
-        type: Number,
-      },
+  props: {
+    rows: {
+      default: 5,
+      type: Number,
     },
+  },
 
-    computed: {
-      height () {
-        return this.rows * 21;
-      },
+  computed: {
+    height() {
+      return this.rows * 21;
     },
+  },
 
-    methods: {
-      getYPos (row, plus) {
-        return plus + ((row - 1) * 22);
-      },
+  methods: {
+    getYPos(row, plus) {
+      return plus + ((row - 1) * 22);
     },
-  };
+  },
+};
 </script>
