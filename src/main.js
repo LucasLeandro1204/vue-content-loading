67422066f89f532186b187fb804dd8c18e654e4d
LucasLeandro1204 <lucasleandro1204@gmail.com>
2019-03-18 02:49:14 -0300

    export only vue content loading and facebook preset

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import VueContentLoading from './components/VueContentLoading.vue';
+import VueContentLoading from './components/VueContentLoading';
 
-import VclCode from './components/presets/Code.vue';
-import VclList from './components/presets/List.vue';
-import VclTable from './components/presets/Table.vue';
-import VclTwitch from './components/presets/Twitch.vue';
-import VclFacebook from './components/presets/Facebook.vue';
-import VclInstagram from './components/presets/Instagram.vue';
-import VclBulletList from './components/presets/BulletList.vue';
+// import VclCode from './components/presets/Code.vue';
+// import VclList from './components/presets/List.vue';
+// import VclTable from './components/presets/Table.vue';
+// import VclTwitch from './components/presets/Twitch.vue';
+import VclFacebook from './components/presets/Facebook';
+// import VclInstagram from './components/presets/Instagram.vue';
+// import VclBulletList from './components/presets/BulletList.vue';
 
 export default VueContentLoading;
 
 export {
-  VclCode,
-  VclList,
-  VclTable,
-  VclTwitch,
+  // VclCode,
+  // VclList,
+  // VclTable,
+  // VclTwitch,
   VclFacebook,
-  VclInstagram,
-  VclBulletList,
+  // VclInstagram,
+  // VclBulletList,
   VueContentLoading,
 };
