c4d6fd305cc81cb23c435fbf05b3e235bf9ebc94
LucasLeandro1204 <lucasleandro1204@gmail.com>
2019-03-17 23:00:29 -0300

    add meta

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,16 @@
 module.exports = {
   title: 'Vue Content Loading',
   description: 'Vue component to easily build (or use presets) SVG loading cards Facebook like',
+
+  head: [
+    ['link', { rel: 'icon', href: '/icon.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+  ],
+
   themeConfig: {
     repo: 'LucasLeandro1204/vue-content-loading',
     repoLabel: 'GitHub',
