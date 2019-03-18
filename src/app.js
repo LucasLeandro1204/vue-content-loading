d8882d8efc72d8c293a83bbf7a0b42aa4f473107
LucasLeandro1204 <lucasleandro1204@gmail.com>
2019-03-17 22:26:11 -0300

    drop color switch and move components to main file

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import * as VclComponents from './core/components';
-import ColorSwitch from './components/ColorSwitch.vue';
-
-Object.keys(VclComponents).forEach(c => Vue.component(c, VclComponents[c]));
-
-Vue.component('ColorSwitch', ColorSwitch);
-
-window.Vue = Vue;
